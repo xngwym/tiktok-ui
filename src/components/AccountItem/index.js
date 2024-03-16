7fc44bd2e58f421edc7c2ca0bf652c0b163b26ef
XuanNguyen <xngwym2482@gmail.com>
2024-03-16 22:50:08 +0700

    Xây dựng UI phần Header dự án Tiktok 02

---
+++ src/components/AccountItem/index.js
@@ -0,0 +1,27 @@
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import classNames from 'classnames/bind';
+import styles from './AccountItem.module.scss';
+
+const cx = classNames.bind(styles);
+
+function AccountItem() {
+    return (
+        <div className={cx('wrapper')}>
+            <img
+                className={cx('avatar')}
+                src="https://img6.thuthuatphanmem.vn/uploads/2022/10/27/anh-avatar-nu-che-mat-anime_084750690.jpg"
+                alt="Hoaa"
+            />
+            <div className={cx('info')}>
+                <p className={cx('name')}>
+                    <span>Nguyen Van A</span>
+                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                </p>
+                <span className={cx('username')}>nguyenvana</span>
+            </div>
+        </div>
+    );
+}
+
+export default AccountItem;
